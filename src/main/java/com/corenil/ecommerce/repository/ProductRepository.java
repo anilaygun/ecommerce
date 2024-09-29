5f07affc1e55987c490db707e4536f721ee4d36c
anilaygun <anil.aygun.37@gmail.com>
2024-09-29 16:15:24 +0300

    ProductRepository refactor1

--- src/main/java/com/corenil/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/corenil/ecommerce/repository/ProductRepository.java
@@ -3,5 +3,13 @@ package com.corenil.ecommerce.repository;
 import com.corenil.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    // 1. Derived Method Naming
+    List<Product> findByNameLikeIgnoreCase(String name);
+
+    Optional<Product> findByNameIgnoreCase(String name);
 }
