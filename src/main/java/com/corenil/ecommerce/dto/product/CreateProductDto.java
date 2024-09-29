ec143114891c825503854d835a1ae064910d0530
anilaygun <anil.aygun.37@gmail.com>
2024-09-29 17:39:58 +0300

    fix DTO annotations

--- src/main/java/com/corenil/ecommerce/dto/product/CreateProductDto.java
+++ src/main/java/com/corenil/ecommerce/dto/product/CreateProductDto.java
@@ -4,9 +4,17 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateProductDto {
 
     @NotNull
--- src/main/java/com/corenil/ecommerce/dto/product/ProductResponseDto.java
+++ src/main/java/com/corenil/ecommerce/dto/product/ProductResponseDto.java
@@ -1,8 +1,16 @@
 package com.corenil.ecommerce.dto.product;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductResponseDto {
     private int id;
     private String name;
